5d55297685b2ec106daaa5ee56752db5e15fe3f8
김수진 <ksj030120@gmail.com>
2022-07-09 01:57:17 +0900

    2_6 수정

--- 2차/2_6.py
+++ 2차/2_6.py
@@ -2,7 +2,7 @@ def solution(floors):
     dist = 0
     length = len(floors)
     for i in range(1, length):
-        if @@@:
+        if floors[i]>=floors[i-1]:
             dist += floors[i] - floors[i-1]
         else:
             dist += floors[i-1] - floors[i] #i-1이 있으면 인덱스 1부터
